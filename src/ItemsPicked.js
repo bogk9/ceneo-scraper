28fdf95d5d355296715273ddbbb35ae66f5e2da4
bogus <bogus.kania@gmail.com>
2022-06-07 23:32:42 +0200

    frontend

---
+++ src/ItemsPicked.js
@@ -0,0 +1,24 @@
+import './ItemsPicked.css'
+
+function ItemsPicked (props) {
+    let itemListElements = props.itemList.map(el => (
+        <div className="oneItem" key={el.id}>
+            <span><a href={"https://www.ceneo.pl/" + el.id} target="_blank">{el.name}</a></span>
+            <button className="delete" onClick={e => props.handleDeleteItem(el.id)}></button>
+        </div>
+    ))
+    return (
+        <div className="flexContainer">
+        <div className="box itemsPicked itemsPickedREAL">
+            
+            <h4 id="boxInfo">Wybrane produkty:</h4>
+            <hr></hr>
+            {itemListElements}
+            {props.itemList.length !== 0 && <button className="button is-primary searchButton" disabled={props.tooLessMuchProducts} onClick={e => props.handleSearchShops()}>Wyszukaj sklepy</button>}
+            {(props.itemList.length !== 0 && props.tooLessMuchProducts) && <span className="tooMuchLessInfo">Liczba produktów musi być większa niż 1 i mniejsza niż 5</span>}
+        </div>
+        </div>
+    )
+}
+
+export default ItemsPicked
---
+++ src/Propositions.js
@@ -0,0 +1,41 @@
+import './Propositions.css'
+import { Transition } from 'react-transition-group'
+
+const duration = 300;
+
+const defaultStyle = {
+  transition: `opacity ${duration}ms ease-in-out`,
+  opacity: 0,
+}
+
+const transitionStyles = {
+  entering: { opacity: 1 },
+  entered:  { opacity: 1 },
+  exiting:  { opacity: 0 },
+  exited:  { opacity: 0 },
+};
+
+function Propositions (props) {
+
+    if (props.propositions.length == 0 && props.zeroProductSearch === false) return (null)
+
+    let propositionsElements = props.propositions.map((el, ind) => (
+        <div className="oneItem" key={el.id}>
+            <span><a href={"https://www.ceneo.pl/" + el.id} target="_blank">{el.name}</a></span>
+            <button className="button is-primary is-small" onClick={e => props.handlePropositionClick(el.name, el.id)}>Dodaj</button>
+        </div>
+    ))
+
+    return (
+            <div className="flexContainer">
+            <div className="box itemsPicked propositions">
+                <h4 id="boxInfo">Który produkt Cię interesuje?</h4>
+                <hr></hr>
+                {propositionsElements}
+                {props.zeroProductSearch && <span className="zeroSearch">Nie znaleziono żadnego produktu o podanej nazwie</span>}
+            </div>
+            </div>
+    )
+}
+
+export default Propositions
