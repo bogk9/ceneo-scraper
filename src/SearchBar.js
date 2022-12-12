dde8b1dbaace38324d13c9e7a394618d469bccd7
bogus <bogus.kania@gmail.com>
2022-12-12 19:38:08 +0100

    Code readability

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,16 +1,34 @@
-import  './SearchBar.css'
+import "./SearchBar.css";
 
-function SearchBar (props) {
-    return (
-        <form onSubmit={(e) => props.handleSubmit(e)}>
-        <input className="input is-primary" type="text" name="productName" placeholder="Nazwa Produktu" value={props.searchInputVal}  onChange={(e) => props.handleChange(e)}/>
-        {props.loading
-          ? <progress className="productProgress progress is-small is-primary" max="100">15%</progress>
-          : <progress className="productProgress progress is-small is-primary" max="100" value="0">15%</progress>
-        }
-        
-        </form>
-    )
+function SearchBar(props) {
+  return (
+    <form onSubmit={(e) => props.handleSubmit(e)}>
+      <input
+        className="input is-primary"
+        type="text"
+        name="productName"
+        placeholder="Nazwa Produktu"
+        value={props.searchInputVal}
+        onChange={(e) => props.handleChange(e)}
+      />
+      {props.loading ? (
+        <progress
+          className="productProgress progress is-small is-primary"
+          max="100"
+        >
+          15%
+        </progress>
+      ) : (
+        <progress
+          className="productProgress progress is-small is-primary"
+          max="100"
+          value="0"
+        >
+          15%
+        </progress>
+      )}
+    </form>
+  );
 }
 
-export default SearchBar
+export default SearchBar;
